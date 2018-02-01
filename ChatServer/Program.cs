8f3daba5d62970030849d49f9a3a46501bdd8f9f
Aaron Schultz <schultzaaron@web.de>
2018-02-01 11:31:59 +0100

    Change port

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -14,7 +14,7 @@ namespace ChatServer {
             // use http://*:8080 to bind to all addresses. 
             // See http://msdn.microsoft.com/en-us/library/system.net.httplistener.aspx 
             // for more information.
-            string url = "http://localhost:8080";
+            string url = "http://localhost:9000";
             using(WebApp.Start(url)) {
                 Console.WriteLine("Server running on {0}", url);
                 Console.ReadLine();
